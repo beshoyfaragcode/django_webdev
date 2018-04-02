886f2903402d647e7bdc0bc2c3945d24440a60f6
Beshoy Farag <beshoyfarag2009@gmail.com>
2018-04-02 15:11:55 -0700

    undo

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-
+from django.core.urlresolvers import reverse
 class Category(models.Model):
  name = models.CharField (max_length = 160)
  slug =  models.SlugField(max_length = 160,unique=True)
