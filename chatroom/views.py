e87e46de1bf44f12171717dfd778f8a78125660c
Beshoy Farag <beshoyfarag2009@gmail.com>
2018-04-02 15:52:58 -0700

    removed pusher

--- chatroom/views.py
+++ chatroom/views.py
@@ -2,17 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from pusher import Pusher
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
-pusher = Pusher(app_id=u'501269', key=u'd5b0dd6e176ebb8783b9', secret=u'85b5a6f33e94fc7ac7f3')
 
-@login_required(login_url='/login/')
+
 def chatroom (request):
   return render (request,'chatroom/chatroom.html')
-@csrf_exempt
-def broadcast(request):
-    pusher.trigger(u'a_channel', u'an_event', {u'name': request.user.username, u'message': request.POST['message']})
-    return HttpResponse("done")
