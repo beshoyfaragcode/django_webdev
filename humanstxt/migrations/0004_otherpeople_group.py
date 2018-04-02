4d1d6b29bd2416d690a7c5990510d37e75d640d2
Beshoy Farag <beshoyfarag2009@gmail.com>
2018-04-02 16:02:50 -0700

    maked migrations

--- humanstxt/migrations/0004_otherpeople_group.py
+++ humanstxt/migrations/0004_otherpeople_group.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='otherpeople',
             name='group',
-            field=models.ForeignKey(blank=True, to='humanstxt.Group', null=True),
+            field=models.ForeignKey(blank=True, to='humanstxt.Group', null=True,on_delete=models.DO_NOTHING),
         ),
     ]
