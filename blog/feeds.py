9655ae618e8da471c3975a7d3f1c5f791b928c2e
Beshoy Farag <beshoyfarag2009@gmail.com>
2018-04-08 18:05:31 -0700

    11th commit

--- blog/feeds.py
+++ blog/feeds.py
@@ -4,24 +4,23 @@ from django.urls import reverse
 from django.utils.feedgenerator import Atom1Feed
 
 class RssSiteNewsFeed(Feed):
-    title =" title"
-    link = "/blog"
-    description = "description"
+    title = Post.title
+    link =  Post.get_absolute_url
+    description =  Post.description
 
     def items(self):
         return Post.objects.order_by('date')
 
-    def item_title(self, item): 
+    def title(self,items): 
         return Post.title
 
-    def item_description(self, item):
+    def description(self, items):
         return Post.description
 
-    def item_link(self, item):
-        return reverse('post', args=[Post.slug])
+    def link(self,items):
+        return  Post.get_absolute_url()
 
-    def link(self, obj):
-        return Post.get_absolute_url()
+  
 
 class AtomSiteNewsFeed(RssSiteNewsFeed):
     feed_type = Atom1Feed
